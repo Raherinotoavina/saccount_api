a894b7f7512c6f8553e81ce3f6f1a1a6ce6aad96
ramasama <raherinotoavinasa@gmail.com>
2025-01-26 07:27:37 +0300

    🦀 add env 🚀

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
-use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
 mod controller;
 mod dto;
 mod routes;
+
+use actix_web::{post, web, App, HttpResponse, HttpServer, Responder};
+use dotenvy::dotenv;
+use std::env;
 
 #[post("/echo")]
 async fn echo(req_body: String) -> impl Responder {
@@ -10,13 +13,20 @@ async fn echo(req_body: String) -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv().ok();
+
+    let port: u16 = env::var("PORT")
+        .unwrap_or("8080".to_string())
+        .parse()
+        .expect("PORT must be a number");
+
     HttpServer::new(|| {
         App::new().service(
             web::scope("/api")
                 .service(web::scope("/user").configure(routes::user_route::user_route_config)),
         )
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
